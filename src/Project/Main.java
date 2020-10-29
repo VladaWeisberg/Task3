21f653db39da9615d4e00e0413169b75dcd2de75
VladaWeisberg <e_nagibina01@mail.ru>
2020-10-29 16:52:48 +0300

    Task_3

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,4 +1,4 @@
-package Project; //96
+package Project;
 
 import java.util.Scanner;
 
@@ -9,27 +9,27 @@ public class Main
     {
         Picture picture = new Picture(
                 new Line(4, -2),
-                new Parabola(6.0/49, -36.0/49, 152.0/49),
-                new Parabola(-1.0/4, -2, -2),
-                new Parabola(-1.0/8, 5.0/4, 7.0/8),
+                new Parabola(6.0 / 49, -36.0 / 49, 152.0 / 49),
+                new Parabola(-1.0 / 4, -2, -2),
+                new Parabola(-1.0 / 8, 5.0 / 4, 7.0 / 8),
                 new Circle(-2, 0, 4));
 
         test(picture);
 
-        if (test(picture))
-        {
+        if (test(picture)) {
+            System.out.println("Test was successful\n");
             startProgram(picture);
-        } else
-        {
+        } else {
             System.out.println("Test failed");
         }
     }
 
     private static void startProgram(Picture picture)
     {
-        double restart = 1;
+        String restart = "yes";
+        String restartGet = "yes";
 
-        while (restart == 1)
+        while (restart == restartGet.intern())
         {
             double x = readDouble("point coordinate 'x'");
             double y = readDouble("point coordinate 'y'");
@@ -40,38 +40,36 @@ public class Main
 
             printColor(color, point);
 
-            restart = readDouble("number 1 if you want to input new coordinates");
+            restartGet = readString();
         }
     }
 
-    private static boolean test(Picture picture)
-    {
-        boolean bol = true;
+    private static boolean test(Picture picture) {
+        boolean check = true;
 
-        TestCase [] cases = new TestCase []{new TestCase(3, 0, SimpleColor.BLUE),
+        TestCase[] testCases = new TestCase[]{new TestCase(3, 0, SimpleColor.BLUE),
                 new TestCase(3, 3, SimpleColor.YELLOW), new TestCase(3, 4, SimpleColor.ORANGE),
                 new TestCase(0, -5, SimpleColor.GRAY), new TestCase(-3, 3, SimpleColor.GREEN),
                 new TestCase(-8, -1, SimpleColor.WHITE)};
 
-        for (int i = 0; i < cases.length; i++)
-        {
-            TestCase testing = cases[i];
+        for (int i = 0; i < testCases.length; i++) {
+            TestCase testing = testCases[i];
 
             Point point = new Point(testing.getX(), testing.getY());
 
             SimpleColor color = picture.getColor(point);
-            SimpleColor rightColor = testing.getRightColor();
-
-            boolean check = checkResult(color, rightColor);
+            SimpleColor expectedColor = testing.getRightColor();
 
             printColor(color, point);
 
-            System.out.printf("%s\n", check);
+            System.out.printf("EXPECTED AREA IS %s\n", expectedColor);
 
-            bol = bol && check;
+            if (color != expectedColor) {
+                check = false;
+            }
         }
 
-        return bol;
+        return check;
     }
 
     private static double readDouble(String name)
@@ -81,14 +79,16 @@ public class Main
         return scanner.nextDouble();
     }
 
+    private static String readString()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Restart? [yes/no] ");
+        return  scanner.next();
+    }
+
     private static void printColor(SimpleColor color, Point point)
     {
         System.out.printf("FOR X = %1$.0f AND Y = %2$.0f IS POINT IN " + color + " AREA \n",
-                            point.getX(), point.getY());
-    }
-
-    private static boolean checkResult(SimpleColor color, SimpleColor rightColor)
-    {
-        return color == rightColor;
+                point.getX(), point.getY());
     }
 }
